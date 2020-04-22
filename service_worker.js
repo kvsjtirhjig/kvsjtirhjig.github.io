d02d05e203c4d9e4d78491196f4ef5c834c5edee
Your Name <you@example.com>
2020-04-23 01:56:23 +0900

    service_worker.js REV

--- service_worker.js
+++ service_worker.js
@@ -1,10 +1,7 @@
 // キャッシュファイルの指定
 var CACHE_NAME = 'pwa-sample-caches';
-//var urlsToCache = [
-//    '/kvsjtirhjig.github.io/',
-//];
 var urlsToCache = [
-    '/',
+    '/kvsjtirhjig.github.io/',
 ];
 
 // インストール処理
